ddfc7bcb293f08eb986ec34a1881858a852949bf
Vishnu-swindia <114577176+Vishnu-swindia@users.noreply.github.com>
2023-07-28 12:42:49 +0530

    .

--- src/Components/CustomCalendar.js
+++ src/Components/CustomCalendar.js
@@ -3,14 +3,13 @@ import {SafeAreaView, ScrollView, StyleSheet, View, Text} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 import MIcon from 'react-native-vector-icons/MaterialIcons';
 import {COLORS} from '../Resources/Resources';
-import { MainJSON } from '../Resources/MainJSON';
+import {MainJSON} from '../Resources/MainJSON';
 
 export default function CustomCalendar(props) {
+  
   const selectedMonth = props.selectedMonth;
   const [currentMonthName, setCurrentMonthName] = useState();
-  const [currentMonth, setCurrentMonth] = useState();
   const [initialDate, setInitialDate] = useState();
-
 
   useEffect(() => {
     if (selectedMonth !== undefined) {
@@ -20,7 +19,9 @@ export default function CustomCalendar(props) {
       setInitialDate(date.toISOString().slice(0, 10));
       // setCurrentMonth(selectedMonth)
     } else {
-      setCurrentMonthName(new Date().toLocaleString('default', {month: 'long'}));
+      setCurrentMonthName(
+        new Date().toLocaleString('default', {month: 'long'}),
+      );
       setInitialDate(new Date().toISOString().slice(0, 10));
     }
   }, [selectedMonth]);
@@ -47,8 +48,6 @@ export default function CustomCalendar(props) {
     }
   };
 
-
-
   const markedDates = {
     [new Date().toISOString().slice(0, 10)]: {
       selected: true,
@@ -57,25 +56,24 @@ export default function CustomCalendar(props) {
     '2023-07-17': {marked: true},
     '2023-07-18': {marked: true},
     '2023-07-19': {marked: true},
-  
   };
-// Function to get all dates between 'fromDate' and 'toDate' (inclusive)
-function getDatesBetween(fromDate, toDate) {
-  const dates = [];
-  const current = new Date(fromDate);
-  const last = new Date(toDate);
+  // Function to get all dates between 'fromDate' and 'toDate' (inclusive)
+  function getDatesBetween(fromDate, toDate) {
+    const dates = [];
+    const current = new Date(fromDate);
+    const last = new Date(toDate);
 
-  while (current <= last) {
-    dates.push(new Date(current).toISOString().slice(0, 10));
-    current.setDate(current.getDate() + 1);
+    while (current <= last) {
+      dates.push(new Date(current).toISOString().slice(0, 10));
+      current.setDate(current.getDate() + 1);
+    }
+
+    return dates;
   }
-
-  return dates;
-}
   // Loop through the 'data' array
   MainJSON.Bookings.bookings[selectedMonth].forEach(booking => {
-    const { fromDate, toDate, bookingDetails } = booking;
-  
+    const {fromDate, toDate, bookingDetails} = booking;
+
     // Create a new object with properties to be added to 'markedDates'
     const bookingObject = {
       marked: true,
@@ -83,10 +81,10 @@ function getDatesBetween(fromDate, toDate) {
       activeOpacity: 0,
       bookingDetails: bookingDetails,
     };
-  
+
     // Get all dates between 'fromDate' and 'toDate'
     const datesInRange = getDatesBetween(fromDate, toDate);
-  
+
     // Add the newly created object to the 'markedDates' object for each date in the range
     datesInRange.forEach(date => {
       markedDates[date] = bookingObject;
@@ -97,13 +95,11 @@ function getDatesBetween(fromDate, toDate) {
     <Calendar
       disableAllTouchEventsForDisabledDays={true}
       onMonthChange={month => {
-      
         const date = new Date(0, month.month, 1);
         const monthName = date.toLocaleString('default', {month: 'long'});
         setCurrentMonthName(monthName);
-      // setCurrentMonth(month.month)
-      props.onChangeMonth(month.month)
-
+        // setCurrentMonth(month.month)
+        props.onChangeMonth(month.month);
       }}
       initialDate={initialDate}
       renderArrow={renderArrow}
